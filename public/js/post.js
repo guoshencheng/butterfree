83e37282b74b0f6fe05d7d0b282c83e017fd97ac
guoshencheng <648772021@qq.com>
2018-02-19 21:47:33 +0800

    post edit submit

--- public/js/post.js
+++ public/js/post.js
@@ -3,5 +3,20 @@ $(function() {
   $('.submit-button').on('click', function() {
     const postForm = $('#post');;
     const params = postForm.serializeObject();
+    $.ajax({
+      method: 'PUT',
+      url: '/api/v1/posts/' + id,
+      data: $.extend({}, params, { id: id })
+    }).then(function(response) {
+      if (response.success) {
+        layer.confirm('修改成功!', {
+          btn: ['确定']
+        }, function(){
+          window.location.href = '/posts';
+        });
+      } else {
+        layer.alert(res.message)
+      }
+    })
   })
 })
