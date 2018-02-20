5793d1ba5759605cc026eeb0c5b1a51732cdc192
guoshencheng <648772021@qq.com>
2018-02-20 12:11:04 +0800

    finish my tips

--- public/js/tips.js
+++ public/js/tips.js
@@ -30,13 +30,8 @@ $(function() {
       }
     }]
   });
-  $('body').on('click', '.fa-edit', function() {
-    var id = $(this).data('id');
-    var tipText;
-    if (id) {
-      tipText = $(this).closest('tr').find('.tipTextContainer').text();
-    }
-    var params = { id: id, tipText: tipText }
+  function createOrEdit(id, tipText) {
+    var params = { id: id || '', tipText: tipText || '' }
     var index = layer.open({
       type: 1,
       title: '编辑tip',
@@ -45,8 +40,49 @@ $(function() {
       area: ['400px', '350px'],
       content: template(params),
       yes: function() {
-        
+        var tipForm = $('#tipform');;
+        var params = tipForm.serializeObject();
+        if (id) {
+          $.ajax({
+            method: 'PUT',
+            url: '/api/v1/tips/' + id,
+            data: params
+          }).then(function(res) {
+            if (res.success) {
+              layer.close(index);
+              table.ajax.reload();
+            } else {
+              layer.alert(res.message)
+            }
+          })
+        } else {
+          $.ajax({
+            method: 'POST',
+            url: '/api/v1/tips',
+            data: params
+          }).then(function(res) {
+            if (res.success) {
+              layer.close(index);
+              table.ajax.reload();
+            } else {
+              layer.alert(res.message)
+            }
+          })
+
+        }
       }
     })
+  }
+  $('body').on('click', '.fa-edit', function() {
+    var id = $(this).data('id');
+    var tipText;
+    if (id) {
+      tipText = $(this).closest('tr').find('.tipTextContainer').text();
+    }
+    createOrEdit(id, tipText)
+  });
+
+  $('body').on('click', '.create-tip', function() {
+    createOrEdit()
   });
 })
